e0f53506df2fc15582b215cca08f01071d71d924
Tony <dubich.t@gmail.com>
2020-05-24 00:40:43 +0300

    commit

---
+++ js/index_help.js
@@ -0,0 +1,85 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+
+  // --------------- function accordion start---------------------
+
+  function accardion(elem) {
+    var accordion = document.querySelector(elem + ' .accordion');
+    var accordionList = document.querySelectorAll(elem + ' .accordion__item');
+    var accordionContent = document.querySelectorAll('.accordion__content');
+
+
+
+    window.addEventListener('resize', function() {
+      
+      accordionList.forEach(function (item) {
+        if(item.classList.contains('selected')){
+          return
+        } else {
+          var content = item.querySelector('.accordion__content');
+          content.style.marginTop = -content.offsetHeight + 'px';
+        }
+      })
+      
+    })
+
+    var countHeight = 0;
+
+    accordionList.forEach(function (item) {
+      countHeight += item.offsetHeight + 'px';
+      accordion.style.minHeight = accordion.offsetHeight - countHeight + 'px';
+      var content = item.querySelector('.accordion__content');
+      content.style.marginTop = -content.offsetHeight + 'px';
+
+      var accordionTitle = item.querySelector('.accordion__title');
+
+      accordionTitle.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        content.style.transition = '0.5s ease';
+        var sibling = item.parentNode.children;
+        var siblingList = [];
+
+        for (var i = 0; i < sibling.length; i++) {
+          if (sibling[i] != item) {
+            siblingList.push(sibling[i])
+          }
+        }
+
+        siblingList.forEach(function (item) {
+
+          if (item.classList.contains('selected')) {
+            item.classList.remove('selected');
+          }
+
+          if (!item.classList.contains('selected')) {
+            item.querySelector('.accordion__content').style.marginTop = -item.querySelector('.accordion__content').offsetHeight + 'px'
+          }
+
+        })
+
+        item.classList.toggle('selected');
+        if (item.classList.contains('selected')) {
+          content.style.marginTop = 0 + 'px';
+        } else {
+          content.style.marginTop = -content.offsetHeight + 'px';
+        }
+
+      })
+    })
+
+  }
+// --------------- function accordion end---------------------
+  accardion('.computer-win');
+
+  
+  // var md = new MobileDetect(window.navigator.userAgent);
+  // console.log(md.os());
+
+  // var div = document.createElement(div);
+  // div.textContent = md.phone();
+
+  // document.body.prepend(div);
+  
+
+})
